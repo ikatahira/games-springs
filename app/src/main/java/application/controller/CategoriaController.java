5635246b0a03e7c024eb63ecee3c0d0dbd72efb7
ikatahira <isaque.katahira@fatec.sp.gov.br>
2025-02-19 17:14:03 +0000

    Editando o CategoriaRepository

--- app/src/main/java/application/controller/CategoriaController.java
+++ app/src/main/java/application/controller/CategoriaController.java
@@ -27,7 +27,35 @@ public class CategoriaRepository{
         return "categoria/list";
     }
 
-    
+    @RequestMapping("/insert")
+    public String insert(){
+        return "categoria/insert";
+    }
+
+    @RequestMapping(value= "/insert", method = ResquestMethod.POST)
+    public String insert (@RequestParam("nome") String nome){
+        Categoria categoria = new Categoria();
+        categoria.setNome(nome);
+        
+        categoriaRepo.save(categoria);
+        return "redirect:/categoria/list";
+    }
+
+    @RequestMapping("/update")
+    public String update(
+        @RequestParam("id") long id,
+        Model ui){
+        
+            Optional<Categoria> categoria = categoriaRepo.findById(id);
+
+            if(categoria.isPresent()){
+                ui.addAttribute("categoria", categoria.get());
+                return "categoria/update";
+            }
+            return "redirect:/categoria/list";
+        }
+    )
+     
 
 
 }
